bbffcb97cb09a6cccd18b698fc0a94fde42c6816
Manuel Aguilar <manguilar22@gmail.com>
2020-02-18 08:50:08 -0700

    first@Tue Feb 18 08:50:08 MST 2020

--- student/index.php
+++ student/index.php
@@ -1,10 +1,13 @@
 <html>
 
+<head>
+    <link rel="stylesheet" href="../css/student.css"/>
+</head>
 
 <body>
 
     <!-- Student Account -->
-
+    <div class="studentForm">
     <form method="post" action="#">
         <label>Name</label> <br/>
         <input type="text" name="firstName" placeholder="First Name Goes Here"/> <br/>
@@ -14,13 +17,20 @@
         <input type="text" name="utepEmail" placeholder="Use student email, please"/> <br/>
         <label>Classification</label> <br/>
         <!-- SELECT GOES HERE -->
+        <select name="classification">
+            <option value="Undergraduate">Undergraduate</option>
+            <option value="Graduate">Graduate</option>
+            <option value="Doctorate">Doctorate</option>
+        </select> <br/>
         <label>Major GPA</label> <br/>
+        <input type="text" name="majorGPA" placeholder="Major GPA"/> <br/>
         <label>Overall GPA</label> <br/>
+        <input type="text" name="overallGPA" placeholder="Overall GPA"/> <br/>
         <label>Password</label> <br/>
         <input type="password" name="password" placeholder="type a secret password"/><br/>
         <button type="submit">Create Account</button>
     </form>
-
+    </div>
 
 </body>
 
