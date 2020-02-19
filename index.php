23bd60bc605110513f5acb3be4174ca8ddd6c906
Manuel Aguilar <manguilar22@gmail.com>
2020-02-19 08:50:23 -0700

    .

--- index.php
+++ index.php
@@ -19,10 +19,17 @@
             <li><a href="/student/index.php">Create an Account</a></li>
         </ol>
     </div>
-	
+
+
+    <?php
+    require "./oop/databaseConnect.php";
+    $db = new DatabaseConnector();
+    $db -> DOCKER_CONNECT("172.17.0.2","root","password","test");
+    echo "print if work";
+    ?>
+
 </body>
 
 <!-- Javascript -->
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" />
 </html>
--- oop/databaseConnect.php
+++ oop/databaseConnect.php
@@ -2,14 +2,22 @@
 
 	class DatabaseConnector { 
 
-		private $utepMySQL = "exampledatabaseserver.cs.utep.edu"; 
-
-
 		public function __constructor(){} 
 
-		public function __constructor($utepMySQL){ %this -> $utepMySQL = $utepMySQL; }	
+		public function set($utepMySQL){ $this -> $utepMySQL = $utepMySQL; }
 
-		public function UTEP_CONNECT($user, $password, $database){
+		public function UTEP_CONNECT($user,$password,$database) {
+			$utepMYSQLAddr = "ilinkserver.cs.utep.edu";
+			$username = $_POST[$user];
+			$usernamePassword = $_POST[$password];
+			$conn = new mysqli($utepMYSQLAddr,$user,$password,$database);
+			if ($conn -> connect_error){
+				die("Failed to Connect to UTEP Database");
+			}
+			return $conn;
+		}
+
+		public function UTEP_CONNECT_TRASH_METHOD($host,$user, $password, $database){
 
 			$connection = mysql_connect($host,$user,$password); 
 			if(!$connection) {
@@ -18,11 +26,15 @@
 			return $connection; 	
 		}
 
-		public function DOCKER_CONNECT($host_uri){}
+		public function DOCKER_CONNECT($host_uri,$username,$password,$database){
+			$connection = new mysqli($host_uri,$username,$password,$database);
+			if ($connection -> connect_error) {
+				die("Failed to connect to docker instance");
+			}
+		}
 
-		public function DOCKER_COMPOSE_CONNECT{}
+		public function DOCKER_COMPOSE_CONNECT(){}
 
 
 	} 
 
-?>
