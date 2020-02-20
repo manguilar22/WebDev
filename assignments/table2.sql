127a067b4ed4dc22ffb6ac8b12ed86c549dceff2
Manuel Aguilar <manguilar22@gmail.com>
2020-02-20 10:16:13 -0700

    broke@Thu Feb 20 10:16:13 MST 2020

---
+++ assignments/table2.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE MyFirstTable (
+
+field1 CHAR(10),
+field2 VARCHAR(10),
+field3 DECIMAL(3,2),
+field4 DATE('m-d-y')
+
+)
+
+INSERT INTO MyFirstTable VALUES ('a','abc',1.11,'2-1-2020'), ('b','aabbcc',2.22,'2-2-2020');
