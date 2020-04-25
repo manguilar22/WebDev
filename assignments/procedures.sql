28705b41291c71f8470868a39a2c7bd15735aaeb
Manuel Aguilar <manguilar22@gmail.com>
2020-04-25 12:52:58 -0600

    .

---
+++ assignments/procedures.sql
@@ -0,0 +1,63 @@
+/*
+    Create New Jobs as a ADMIN
+ */
+
+
+DELIMITER //
+CREATE PROCEDURE new_job_role(
+
+)
+BEGIN
+INSERT INTO role(RjobTitle,RclassName,RclassCRN) VALUES (
+            '$jobTitle',
+            '$className',
+            '$classCRN'
+            );
+END; //
+DELIMITER ;
+
+/*
+	Insert student record to database
+*/
+
+DELIMITER //
+CREATE PROCEDURE new_student(
+	IN firstName CHAR(20),
+	IN middleName CHAR(20),
+	IN lastName CHAR(20),
+	IN email VARCHAR(20),
+	IN classification VARCHAR(20),
+	IN ResidencyStatus VARCHAR(20),
+	IN gender VARCHAR(1),
+	IN majorGPA DECIMAL(4,2),
+	IN overallGPA DECIMAL(4,2),
+	IN password VARCHAR(100)
+)
+BEGIN 
+	INSERT INTO student(
+        Sfname,
+        Smname,
+        Slname,
+        Semail,
+        Sclassification,
+        Sresidency_status,
+        Sgender,
+        Smajor_gpa,
+        Sover_all_gpa,
+        Spassword
+    ) VALUES (
+        firstName,
+        middleName,
+        lastName,
+        email,
+        classification,
+        residencyStatus,
+        gender,
+        majorGPA,
+        overallGPA,
+        password
+    );
+END; // 
+DELIMITER ;
+
+CALL new_student("first","","last","demo","undergraduate","in-state","M",3.33,2.22,"demo1");
---
+++ public/jobs/student/student_profile.php
@@ -0,0 +1,91 @@
+<?php session_start(); ?>
+<?php
+
+//include "design_template.html";
+
+// Connect to UTEP Database
+require_once "../../oop/databaseConnect.php";
+$database = new DatabaseConnector();
+$conn = $database->connect();
+
+$username = $_SESSION["user"];
+
+// This will return a row with all attributes associated with the email.
+$findStudentQuery = "SELECT * FROM student WHERE Semail LIKE '$username'";
+
+
+$studentData = $conn -> query($findStudentQuery) -> fetch_array();
+
+// Retrieve what you want.
+$firstName  = $studentData["Sfname"];
+$middleName = $studentData["Smname"];
+$lastName   = $studentData["Slname"];
+$classification = $studentData["Sclassification"];
+$residency  = $studentData["Sresidency-status"];
+$overallGPA = $studentData["Sover_all_gpa"];
+$majorGPA   = $studentData["Smajor_gpa"];
+$email      = $studentData["Semail"];
+$gender     = $studentData["sgender"];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<!-- LINK STYLESHEET -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/design_style.css"/>
+    <link rel="stylesheet" href="../../css/student_profile_style.css"/>
+    <title>Profile</title>
+</head>
+
+<body>
+
+<div class="box">
+<p>Hi <?php echo $firstName . " " . $lastName ?></p>
+</div>
+
+
+<!-- UTEP LOGO ON LEFT SIDE OF HEADER-->
+<div class="navigation-bar">
+    <img src="../../css/img/utep_logo.png">
+</div>
+<!-- TITLE IN HEADER -->
+<div class="header">
+    <h1>STUDENT PROFILE</h1>
+</div>
+
+<!-- NAGIVATION BAR -->
+<div class="navigation">
+    <ul>
+        <li><a href="../../student/login.php">LOG OUT<?php session_destroy();?></a></li>
+        <li><a href="../../index.php">BACK</a></li>
+    </ul>
+</div>
+
+<div class="left">
+    <h3>RESOURCES</h3><hr></hr>
+    <div class="leftBox">
+        <div class = "sidenav">
+            <div class="links">
+                <a href="../application.php">Fill Application</a></br>
+            </div>
+            <div class="links">
+                <a href="./create_account.php">View Status</a>
+            </div>
+            <div class="links">
+                <a href="./create_account.php">Edit Profile</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="div1">
+
+</div>
+
+
+
+</body>
+</html>
